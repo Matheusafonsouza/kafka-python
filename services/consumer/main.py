009a79fe10be18a5323e24f9462b7fc9859736fe
matheusafonsouza <matheusafonsouza@gmail.com>
2022-04-11 17:03:02 -0300

    service for consume kafka messages

--- services/consumer/main.py
+++ services/consumer/main.py
@@ -1,17 +1,23 @@
-from common.logger import log
 from sys import exit
 
-def process():
-    pass
+from adapters.kafka.services import Consumer
+from common.logger import log
+
+
+def callback(message):
+    msg = (
+        f"{message.topic}:{message.partition}:{message.offset}: "
+        f"{message.key=} {message.value=}"
+    )
+    log.info(msg)
+
 
 if __name__ == "__main__":
     log.info("Starting consumer service.")
     
     try:
-        while True:
-            process()
-    except Exception:
-        pass
-    finally:
+        Consumer(topics=["test-topic"], callback=callback).start()
+    except Exception as exception:
+        log.exception(exception)
         log.info("Stoping consumer service.")
         exit()
